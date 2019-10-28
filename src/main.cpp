8b1b18fca7fd143ea1340d16bc63cd7ffd19021e
Justin Champagne <jchampagne@email.arizona.edu>
2019-10-28 14:42:56 -0700

    Updated main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -8,6 +8,31 @@
  * for the exception key one-line code such as checking the state of the pin.
  */
 
+/* Procedure:
+ * Include relevant header files
+ * Define states for state machine
+ * Initialize states
+ * 
+ * Inside Main:
+ * Initialize everything else: pins, timers, sei()
+ * Implement state machine
+ * 
+ * (outside main)
+ * Implement ISRs
+ * 
+ */
+
+//#include <Arduino.h> // not allowed except for debugging
+#include <avr/interrupt.h> // for ISR's
+#include <avr/io.h>
+#include "ADC.h"
+#include "PWM.h"
+#include "Switch.h"
+#include "Timer.h"
+
+/*
+ *  Define a set of states that can be used in the state machine using a 
+ */
 
 int main(){
 
